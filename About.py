538de6bf9bb195bd637a9e30eff79081a29171d0
Edima <edimaudo@gmail.com>
2025-11-14 18:05:04 +0000

    created about.py

---
+++ About.py
@@ -0,0 +1,37 @@
+from utils import *
+
+st.title(APP_NAME)
+st.header(ABOUT_HEADER)
+
+st.markdown(
+    """
+Ever wondered what separates a podium finish from the rest of the pack? It's more than just speed—it's strategy, precision, and the power of data.
+This is your chance to work with real, high-stakes racing data from past Toyota victories. We're calling on developers, data scientists, students, strategists, and superfans to dig into the telemetry, lap times, and sensor data to build something extraordinary. Whether you're writing code or building the ultimate data-driven analysis, your insights could shape the future of racing. Join Hack the Track and show us what's possible when passion meets performance. The checkered flag is waiting 🏁
+
+### What is Toyota Racing Development (TRD?
+Toyota Racing Development (TRD) is Toyota’s in-house motorsports division. It’s responsible for designing, engineering, and supporting Toyota’s racing efforts across different disciplines, as well as developing performance parts for production vehicles
+
+### What is the GR Cup?
+The GR Cup is Toyota Gazoo Racing’s single-make, spec-series championship in the U.S. designed to celebrate car culture and deliver close, driver-centric racing. Every competitor races in identically prepared Toyota GR86 cars, ensuring the competition is decided by driver skill, team execution, and strategy—not equipment.  ￼
+
+### Objective
+Design the solution focused on one of these areas:
+
+**Driver Training & Insights**: Create a tool or analysis that could help a driver identify areas for improvement, optimize their racing line, or better understand their performance.
+
+**Pre-Event Prediction**: Can you predict the future? Develop a model to forecast qualifying results, race pace, tire degradation, or other key performance indicators before the green flag drops.
+
+**Post-Event Analysis**: Go beyond the final standings. Create a dashboard or report that tells the story of the race, revealing key moments and strategic decisions that led to the outcome
+
+**Wildcard**: Have a brilliant idea that doesn’t fit the categories above? Surprise us! This category is for the most creative and out-of-the-box applications of racing data.
+
+### JUDGING CRITERIA
+**Application of the TRD Datasets**:Does the project apply the datasets in an effective way, fitting with the categories? Does the project showcase the datasets uniquely?
+
+**Design**:Is the user experience and design of the project well thought out? Is there a balanced blend of frontend and backend in the software?
+
+**Potential Impact**:How big of an impact could the project have on the Toyota Racing community? How big of an impact could it have beyond the target community?
+
+**Quality of the Idea**:How creative and unique is the project? Does the concept exist already? If so, how much does the project improve on it?
+    """
+)
