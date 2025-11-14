36b642b20f2d934df05745c9ee52acf8b22b0473
Edima <edimaudo@gmail.com>
2025-11-14 18:05:48 +0000

    updated the app py section

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
-import streamlit as st
+from utils import *
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+pg = st.navigation([
+    st.Page("About.py"),
+    st.Page("Driver_Insights.py"),
+    st.Page("Paddock.py"),
+    st.Page("Checkered_Flag.py"),
+])
+pg.run()
