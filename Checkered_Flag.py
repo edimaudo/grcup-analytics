da58fb3c7278f9728f005a270c1aa6b6712e63bf
Edima <edimaudo@gmail.com>
2025-11-17 17:30:39 +0000

    update

---
+++ Checkered_Flag.py
@@ -0,0 +1,10 @@
+from utils import *
+from data import *
+
+st.title(APP_NAME)
+st.header(POST_RACE_HEADER)
+
+#Fastest Lap Distribution - Histogram showing when drivers set their fastest laps
+#Gap Analysis - Waterfall chart showing time gaps between positions
+#DNF Analysis - Bar chart of incomplete races
+#Performance vs Weather - Scatter plot correlating lap times with track temperature
---
+++ Paddock.py
@@ -0,0 +1,9 @@
+from utils import *
+from data import *
+
+st.title(APP_NAME)
+st.header(PRE_RACE_HEADER)
+
+# Visualizations
+#Weather Timeline - Multi-panel view of temperature, humidity, pressure, and wind
+#Wind Polar Plot - Directional wind speed visualization
